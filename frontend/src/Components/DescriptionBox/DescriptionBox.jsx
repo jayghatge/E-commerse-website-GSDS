8bc0ec9244c67f4666b617e8ef38bb6d51f57c76
jayghatge <jayghatge4@gmail.com>
2024-04-11 14:51:09 +0530

    Added frontend backend files

---
+++ frontend/src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>An ecommerce website is your digital storefront on the internet. 
+           It facilitates the transaction between a buyer and seller. 
+           It is the virtual space where you showcase products, and online customers make selections. 
+           Your website acts as the product shelves, sales staff, and cash register of your online business channel.</p>
+           <p>
+           An eCommerce website is an online destination where buyers shop for goods and sellers offer products and services. 
+           It’s the hub of information about a company and what they sell. On an eCommerce website, you’ll find product listings, eCommerce blog content, company history, and contact information.
+           You can sell just about anything through an eCommerce site. 
+           Examples include recreational equipment, car and motorcycle parts, household goods, clothing and jewelry, and food and drinks. 
+           If you need help coming up with additional ideas, consider these wholesale items to sell or high demand products.
+           </p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
